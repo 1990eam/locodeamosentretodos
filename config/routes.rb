6258ed206e2541bf54d19bc21e0a010a02edbba5
Elian Magni <magni.elian@gmail.com>
2020-08-01 15:48:09 -0300

    completed Application#new controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get 'application_requests/new'
-  get 'application_requests/create'
-  get 'applications/new'
-  get 'applications/create'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_01_175820) do
+ActiveRecord::Schema.define(version: 2020_08_01_180945) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2020_08_01_175820) do
     t.bigint "role_id", null: false
     t.bigint "user_id", null: false
     t.string "status"
+    t.text "about_me"
     t.index ["role_id"], name: "index_application_requests_on_role_id"
     t.index ["user_id"], name: "index_application_requests_on_user_id"
   end
