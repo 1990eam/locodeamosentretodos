5724eaf7e7133cb4bf42252d91aff1f832d504f1
Tomas Hergott <tomashergott@gmail.com>
2020-08-01 14:53:08 -0300

    Validaciones finalizadas hasta desarrollar más las user stories

--- app/models/role.rb
+++ app/models/role.rb
@@ -2,5 +2,6 @@ class Role < ApplicationRecord
   belongs_to :project
   has_many :collaborators
   has_many :users, through: :collaborators
+  has_many :requirements
   validates :name, presence: true
 end
