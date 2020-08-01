c6c17a1cadc533753684ddc52c35ba23f2736790
Pablo Andrizzi <pandrizzi45@gmail.com>
2020-08-01 10:57:39 -0300

    added responsive footer

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # resources :users
-  resources :projects, only: [:new,:create, :index, :show]
+  resources :projects, only: [:new, :create, :index, :show]
 
 end
