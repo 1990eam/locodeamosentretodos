d7c43f11ad443ef3a781b59813abbb80cbf584ec
Tomas Hergott <tomashergott@gmail.com>
2020-07-31 22:14:25 -0300

    Primeras validaciones para el modelo Project

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,11 @@
 class Project < ApplicationRecord
   belongs_to :user
+  has_many :roles, dependent: :destroy
+  validates :name, presence: true
+  validates :description, presence: true, length: { in: 6..20 }
+  validates :description, presence: true
+  validates :name, uniqueness: true
+
+
+
 end
