94d96646947577278868025eba54fc4bd580fe15
Tomas Hergott <tomashergott@gmail.com>
2020-07-31 22:30:54 -0300

    Foto default para el modelo Project

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,11 +1,11 @@
 class Project < ApplicationRecord
   belongs_to :user
   has_many :roles, dependent: :destroy
+  has_one_attached :photo
   validates :name, presence: true
   validates :description, presence: true, length: { in: 6..20 }
   validates :description, presence: true
   validates :name, uniqueness: true
-
 
 
 end
