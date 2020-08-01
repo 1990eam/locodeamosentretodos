d7ebacca75a17a0069bdd741060b299971996436
Pablo Andrizzi <pandrizzi45@gmail.com>
2020-08-01 16:05:36 -0300

    scss nesting changes

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,6 +24,7 @@ require("channels")
 
 // External imports
 import "bootstrap";
+import AOS from "aos";
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
@@ -33,4 +34,5 @@ import '../components/banner.js';
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+  AOS.init();
 });
