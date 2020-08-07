eb3c604fa5b7992e207b926d53d17a356ab309ea
Elian Magni <magni.elian@gmail.com>
2020-08-07 18:37:27 -0300

    added description to role. basic layout for role cards in New Role View

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_04_230701) do
+ActiveRecord::Schema.define(version: 2020_08_07_213352) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -80,6 +80,7 @@ ActiveRecord::Schema.define(version: 2020_08_04_230701) do
   create_table "roles", force: :cascade do |t|
     t.string "name"
     t.bigint "project_id", null: false
+    t.string "description"
     t.index ["project_id"], name: "index_roles_on_project_id"
   end
 
