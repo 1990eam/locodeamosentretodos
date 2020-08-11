25aa5b43a2f35c683ea7d0fc61fb210e039afe37
Pablo Andrizzi <pandrizzi45@gmail.com>
2020-08-11 19:44:01 -0300

    added icons and technologies to the card

--- app/helpers/index_card_helper.rb
+++ app/helpers/index_card_helper.rb
@@ -9,8 +9,39 @@ module IndexCardHelper
       "<i class='fab fa-facebook-square'></i>"
     when url.match?(/instagram/i)
       "<i class='fab fa-instagram-square'></i>"
+    when url.match?(/bitbucket/i)
+      "<i class='fab fa-bitbucket'></i>"
+    when url.match?(/linkedin/i)
+      "<i class='fab fa-linkedin'></i>"
+    when url.match?(/twitter/i)
+      "<i class='fab fa-twitter-square'></i>"
     else
       "<i class='fas fa-globe'></i>"
+    end
+  end
+
+  def pick_tech_icon(tech_name)
+    return "" if tech_name.nil?
+
+    case
+    when tech_name.match?(/Ruby/i)
+      "<i class='fab fa-sketch'style='color: red;'></i>"
+    when tech_name.match?(/Javascript/i)
+      "<i class='fab fa-js-square'style='color:teal;'></i>"
+    when tech_name.match?(/CSS/i)
+      "<i class='fab fa-css3-alt'style='color: blue;'></i>"
+    when tech_name.match?(/HTML/i)
+      "<i class='fab fa-html5'style='color: orange;'>"
+    when tech_name.match?(/Java/i)
+      "<i class='fab fa-java'style='color: blue;'></i>"
+    when tech_name.match?(/Python/i)
+      "<i class='fab fa-python'></i>"
+    when tech_name.match?(/React/i)
+      "<i class='fab fa-react'style='color:teal;'></i>"
+    when tech_name.match?(/Plain Old Violence/i)
+      "<i class='fas fa-hand-rock'></i>"
+    else
+      "<i class='fas fa-laptop-code'></i>"
     end
   end
 end
