aac0142af4f7c39d627ee3cb879095b9e141fa97
Elian Magni <magni.elian@gmail.com>
2020-08-13 20:28:42 -0300

    fixed bug where it crashed if user was not logged in

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,7 +3,9 @@ class ProjectsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index]
 
   def index
-    @user = current_user
+    if user_signed_in?
+      @user = current_user
+    end
     @projects = policy_scope(Project)
   end
 
