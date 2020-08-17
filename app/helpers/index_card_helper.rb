2c6b27f792aab32dabb8bb154ffd3bdf2723b805
Pablo Andrizzi <pandrizzi45@gmail.com>
2020-08-17 00:46:34 -0300

    más magia de seeds y colores más pro para las tecnologías

--- app/helpers/index_card_helper.rb
+++ app/helpers/index_card_helper.rb
@@ -24,12 +24,22 @@ module IndexCardHelper
     end
   end
 
+  def shorten_url(url)
+    return "" if url.nil?
+
+    if url.match?(Regexp.union("github", "facebook", "instagram", "bitbucket", "linkedin", "twitter", "wikipedia"))
+      url.match(/\/(.*)/)[1]
+    else
+      url
+    end
+  end
+
   def pick_tech_icon(tech_name)
     return "" if tech_name.nil?
 
     case
     when tech_name.match?(/Ruby/i)
-      "<i class='fab fa-sketch'style='color: red;'></i>"
+      "<i class='fab fa-sketch red'style='color: red;'></i>"
     when tech_name.match?(/Javascript/i)
       "<i class='fab fa-js-square'style='color:teal;'></i>"
     when tech_name.match?(/CSS/i)
@@ -37,37 +47,37 @@ module IndexCardHelper
     when tech_name.match?(/HTML/i)
       "<i class='fab fa-html5'style='color: orange;'>"
     when tech_name.match?(/Java/i)
-      "<i class='fab fa-java'style='color: blue;'></i>"
+      "<i class='fab fa-java'style='color: royalblue;'></i>"
     when tech_name.match?(/Python/i)
-      "<i class='fab fa-python'></i>"
+      "<i class='fab fa-python'style='color: seagreen;'></i>"
     when tech_name.match?(/React/i)
-      "<i class='fab fa-react'style='color:teal;'></i>"
+      "<i class='fab fa-react'style='color:mediumturquoise;'></i>"
     when tech_name.match?(/Violence/i)
       "<i class='fas fa-hand-rock'></i>"
     when tech_name.match?(/Intimidation/i)
-      "<i class='fas fa-dumbbell'></i>"
+      "<i class='fas fa-dumbbell' style='color: maroon;'></i>"
     when tech_name.match?(/Rails/i)
-      "<i class='fas fa-train'></i>"
+      "<i class='fas fa-train' style='color: orangered;'></i>"
     when tech_name.match?(/People skills/i)
-      "<i class='fas fa-people-arrows'></i>"
+      "<i class='fas fa-people-arrows' style='color: paleturquoise;'></i>"
     when tech_name.match?(/Agile/i)
-      "<i class='fas fa-tasks'></i>"
+      "<i class='fas fa-tasks' style='color: lightslategrey;'></i>"
     when tech_name.match?(/Scrum/i)
-      "<i class='fas fa-tasks'></i>"
+      "<i class='fas fa-tasks' style='color: silver;'></i>"
     when tech_name.match?(/Accounting/i)
-      "<i class='fas fa-calculator'></i>"
+      "<i class='fas fa-calculator' style='color: brown;'></i>"
     when tech_name.match?(/Nutrition/i)
-      "<i class='fas fa-utensils'></i>"
+      "<i class='fas fa-utensils' style='color: yellowgreen;'></i>"
     when tech_name.match?(/Health/i)
-      "<i class='far fa-hospital'></i>"
+      "<i class='far fa-hospital' style='color: orchid;'></i>"
     when tech_name.match?(/AI/i)
-      "<i class='fas fa-brain'></i>"
+      "<i class='fas fa-brain' style='color: lightpink;'></i>"
     when tech_name.match?(/Logistics/i)
-      "<i class='fas fa-people-carry'></i>"
+      "<i class='fas fa-people-carry' style='color: coral;'></i>"
     when tech_name.match?(/HR/i)
-      "<i class='fas fa-users'></i>"
+      "<i class='fas fa-users' style='color: limegreen;'></i>"
     when tech_name.match?(/Data Science/i)
-      "<i class='fas fa-flask'></i>"
+      "<i class='fas fa-flask' style='color: purple;'></i>"
     else
       "<i class='fas fa-laptop-code'></i>"
     end
