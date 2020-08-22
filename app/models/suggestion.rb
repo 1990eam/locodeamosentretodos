57add7cce1a3fe4ed887dd39de5b30a35c4f6348
Elian Magni <magni.elian@gmail.com>
2020-08-22 11:12:59 -0300

    fixed path for application request redirect. some styling

--- app/models/suggestion.rb
+++ app/models/suggestion.rb
@@ -2,7 +2,8 @@ class Suggestion < ApplicationRecord
   belongs_to :collaborator
   belongs_to :project
   has_many :suggestion_votes
-  validates :description, presence: :true
+  validates :description, presence: :true, length: { maximum: 100, minimum: 10,
+    too_long: "%{count} es el màximo de caracteres. Se breve al describir la feature", too_short: "%{count} es el mìnimo de caracteres." }
 
   def average_rating
     ratings = self.suggestion_votes
