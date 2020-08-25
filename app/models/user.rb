f3fff016f73ba92aa3e71f16f476882d2e0fd310
Tomas Hergott <tomashergott@gmail.com>
2020-08-25 19:09:31 -0300

    Login con github casi funcionando

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  devise :omniauthable, omniauth_providers: [:github]
   has_many :projects, dependent: :destroy
   has_many :collaborators, dependent: :destroy
   has_many :roles, through: :collaborators
@@ -19,5 +20,26 @@ class User < ApplicationRecord
     unless photo.attached?
       photo.attach(io: File.open(Dir.getwd + "/app/assets/images/default-user-img.png"), filename: "default.png", content_type: 'image/png')
     end
+  end
+
+  def self.find_for_github_oauth(auth)
+    user_params = auth.slice("provider", "uid")
+    user_params.merge! auth.info.slice("email", "first_name", "last_name")
+    user_params[:github_picture_url] = auth.info.image
+    user_params[:token] = auth.credentials.token
+    user_params[:token_expiry] = auth.credentials.expires_at
+    user_params = user_params.to_h
+
+    user = User.find_by(provider: auth.provider, uid: auth.uid)
+    user ||= User.find_by(email: auth.info.email) # User did a regular sign up in the past.
+    if user
+      user.update(user_params)
+    else
+      user = User.new(user_params)
+      user.password = Devise.friendly_token[0, 20] # Fake password for validation
+      user.save
+    end
+
+    return user
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,8 @@ Rails.application.routes.draw do
   #       }
   #     end
   devise_for :users, controllers: {
-    registrations: "users/registrations"
+    registrations: "users/registrations",
+    omniauth_callbacks: 'users/omniauth_callbacks'
   }
 
   resources :users do
