818810c1ebc6db46ad74789ccbec40691f77734c
Pablo Andrizzi <pandrizzi45@gmail.com>
2020-08-25 01:56:51 -0300

    new index 90% done, reworked colours and idk how much more shite

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_24_194808) do
+ActiveRecord::Schema.define(version: 2020_08_25_024338) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -167,6 +167,7 @@ ActiveRecord::Schema.define(version: 2020_08_24_194808) do
     t.string "first_name"
     t.string "last_name"
     t.boolean "admin", default: false
+    t.string "description"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
