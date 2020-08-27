2bad69c1936993a3efcd478c3cdc7150d0115a4e
Tomas Hergott <tomashergott@gmail.com>
2020-08-27 19:21:30 -0300

    Se pueden eliminar proyectos. Se quitó la asociación de :suggestion en el modelo Collaborator

--- app/models/collaborator.rb
+++ app/models/collaborator.rb
@@ -1,7 +1,7 @@
 class Collaborator < ApplicationRecord
   belongs_to :role
   belongs_to :user
-  has_many :suggestions, through: :projects, dependent: :destroy
+  has_many :suggestions
   has_many :suggestion_votes, dependent: :destroy
   validates :status, presence: true, inclusion: { in: ["active", "paused", "finished", "removed"] }
 end
